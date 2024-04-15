85a6d8f159e782e3768fc1188fd3af48c64d0503
Vadym <v266289896@gmail.com>
2024-04-15 11:22:09 +0300

    ondrop reset button

--- first-project-react/src/components/Banner/Banner.jsx
+++ first-project-react/src/components/Banner/Banner.jsx
@@ -1,11 +1,4 @@
 // // * Components
-// import styles from './Banner.module.css';
-
-// const Banner = () => {
-//   return <span className={styles.banner}>Full-width banner image </span>;
-// };
-
-// export default Banner;
 
 // * Components
 import { useState } from 'react';
@@ -14,18 +7,54 @@ import { useState } from 'react';
 import styles from './Banner.module.css';
 
 const Banner = () => {
-  const [value, setValue] = useState('Default value ');
+  const [value, setValue] = useState('Default value');
+
   const onChangeEvent = (e) => {
     const v = e.target.value;
     setValue(v);
   };
 
+  const resetInput = () => {
+    setValue('');
+  };
+
   return (
     <div className={styles.banner}>
       <h4>Value: {value}</h4>
-      <input className={styles.input} type="text" onInput={onChangeEvent} />
+      <input
+        className={styles.input}
+        type="text"
+        value={value}
+        onInput={onChangeEvent}
+      />
+      <button onClick={resetInput}>Reset</button>
     </div>
   );
 };
 
 export default Banner;
+
+// import styles from './Banner.module.css';
+
+// const Banner = () => {
+//   return <span className={styles.banner}>Full-width banner image </span>;
+// };
+
+// export default Banner;
+
+// const Banner = () => {
+//   const [value, setValue] = useState('Default value ');
+//   const onChangeEvent = (e) => {
+//     const v = e.target.value;
+//     setValue(v);
+//   };
+
+//   return (
+//     <div className={styles.banner}>
+//       <h4>Value: {value}</h4>
+//       <input className={styles.input} type="text" onInput={onChangeEvent} />
+//     </div>
+//   );
+// };
+
+// export default Banner;
